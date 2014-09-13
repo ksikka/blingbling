ce74f01e3ef0c69d726fed47bffa1fcc82e87e9b
Jacqueline Xu <jxu703@gmail.com>
2014-09-13 19:48:16 -0400

    fixed sound

--- serialint.py
+++ serialint.py
@@ -1,11 +1,6 @@
 
 import time
 import serial
-import sound
-
-
-s = Sound() 
-s.read('sound.wav') 
 
 
 start = time.time()
@@ -15,7 +10,6 @@ ser = serial.Serial('/dev/tty.usbserial-AH02M63N',9600)
 visited = []
 i = 0
 
-s.play()
 while(1):
 	for beat in beats[i:]:
 		current = time.time()
