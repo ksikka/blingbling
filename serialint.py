c94338c72c4709760fd83c3111b3a619a02e068e
Jacqueline Xu <jxu703@gmail.com>
2014-09-13 19:46:54 -0400

    faster beats

--- serialint.py
+++ serialint.py
@@ -1,18 +1,27 @@
 
 import time
 import serial
+import sound
+
+
+s = Sound() 
+s.read('sound.wav') 
+
 
 start = time.time()
 f = open('beat.txt','r')
 beats = eval(f.read())
 ser = serial.Serial('/dev/tty.usbserial-AH02M63N',9600)
 visited = []
+i = 0
 
+s.play()
 while(1):
-	for beat in beats:
+	for beat in beats[i:]:
 		current = time.time()
 		if current - start > beat:
 			if beat not in visited:
 				visited.append(beat)
+				i += 1
 				print(beat)
 				ser.write('5')
